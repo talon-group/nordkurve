8a39f1c7ee610294c488c1397f365c9a9a22d3d4
Liam Arbuckle <liam@skinetics.tech>
2023-12-12 13:04:58 +0100

    🪄🧠 ↝ Fixing some linting issues

--- app/store/[id]/page.tsx
+++ app/store/[id]/page.tsx
@@ -1,28 +1,8 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { createClient } from "@/app/lib/supabase/client";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { revalidatePath, unstable_noStore as noStore } from "next/cache";
 import { Section } from "@/app/components/Section/Section";
-import { useRouter } from "next/navigation";
-
-async function getData({ userId, noteId }: { userId: string; noteId: string }) {
-    noStore();
-    // const data = await prisma.note.findUnique({
-    //   where: {
-    //     id: noteId,
-    //     userId: userId,
-    //   },
-    //   select: {
-    //     title: true,
-    //     description: true,
-    //     id: true,
-    //   },
-    // });
-  
-    // return data;
-};
 
 type Product = {
     id: number;
@@ -34,16 +14,8 @@ type Product = {
     created_at: string;
 };
 
-export default function DynamicShopItemRoute({
-    params,
-}: {
-    params: { id: string };
-}) {
-    const router = useRouter();
+export default function DynamicShopItemRoute({ params }: { params: { id: string } }) {
     const supabase = createClient();
-    const { id: itemId } = router.query;
-    const { getUser } = getKindeServerSession();
-
     const [itemData, setItemData] = useState<Product | undefined>(undefined);
 
     useEffect(() => {
@@ -52,7 +24,7 @@ export default function DynamicShopItemRoute({
                 const { data, error } = await supabase
                     .from("storeItems")
                     .select("*")
-                    .eq("id", itemId)
+                    .eq("id", params.id)
                     .single();
                 if (error) {
                     console.error("Error fetching item data:", error);
@@ -65,10 +37,10 @@ export default function DynamicShopItemRoute({
             }
         };
 
-        if (itemId) {
+        if (params.id) {
             fetchItemData();
         }
-    }, [supabase, itemId]);
+    }, [supabase, params.id]);
 
     return (
         <>
