359c901b4f666bdb1423b83eb9e8121d5a1bed0c
Liam Arbuckle <liam@skinetics.tech>
2024-02-22 14:59:09 +0800

    🍴🌠 ↝ No more hollywood endings

--- src/libs/auth/providers/auth-providers.ts
+++
@@ -1,4 +0,0 @@
-import { getGoogleProvider } from "./google-provider";
-import { getEmailProvider } from "./email-provider";
-
-export const providers = [getEmailProvider(), getGoogleProvider()];
--- src/libs/auth/providers/email-provider.ts
+++
@@ -1,9 +0,0 @@
-import EmailProvider from "next-auth/providers/email";
-
-import { mailConfig } from "@/config/mail-config";
-
-export const getEmailProvider = () =>
-  EmailProvider({
-    server: process.env.EMAIL_SERVER,
-    from: mailConfig.fromAdmin,
-  });
