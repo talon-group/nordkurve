580ef19fedeec9ce6ff5ed4d74fb62219068f2a1
Liam Arbuckle <liam@skinetics.tech>
2024-02-24 15:02:32 +0800

    🐣🧑🏻 ↝ a day at the beach

--- src/app/(admin)/admin/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminDashboardPage() {
-  return (
-    <h1 className="text-xl font-extrabold leading-none tracking-tight md:text-xl lg:text-2xl">
-      Admin Dashboard
-    </h1>
-  );
-}
