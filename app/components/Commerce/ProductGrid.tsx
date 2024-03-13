0b2f8f217facc6872ac678c7f8a498508796213e
Liam Arbuckle <liam@skinetics.tech>
2024-03-13 13:51:50 +0800

    🥎😰 ↝  [ N12-4 N12-5 ]: fetching items from db

--- app/components/Commerce/ProductGrid.tsx
+++ app/components/Commerce/ProductGrid.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const products = [
     {
@@ -32,26 +35,53 @@ const products = [
 ]
 
 export default function AllProductsGrid() {
-    return (
-        <div className="bg-white">
-          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-            <h2 className="sr-only">Products</h2>
-    
-            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {products.map((product) => (
-                <a key={product.id} href={product.href} className="group">
-                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                    <img
-                      src={product.imageSrc}
-                      className="h-full w-full object-cover object-center group-hover:opacity-75"
-                    />
-                  </div>
-                  <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                  <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-                </a>
-              ))}
-            </div>
+  const supabase = useSupabaseClient();
+  const [storeItems, setStoreItems] = useState([]);
+
+  const fetchShopItems = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("storeItems")
+        .select("*")
+
+      if (data) {
+        setStoreItems(data);
+        console.log(storeItems);
+      };
+
+      if (error) {
+        throw error;
+        return;
+      };
+    } catch (error: any) {
+      console.error('Hello' + error.message);
+    };
+  };
+
+  useEffect(() => {
+    fetchShopItems();
+  }, [supabase])
+
+  return (
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+  
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <a key={product.id} href={product.href} className="group">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                  <img
+                    src={product.imageSrc}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+              </a>
+            ))}
           </div>
         </div>
-    )
+      </div>
+  );
 }
